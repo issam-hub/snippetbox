3e709fabfbe97d2297db0cd9fe88725f7cfc638a
issam_hub <issam.hosni@univ-tiaret.dz>
2024-10-09 23:00:53 +0200

    feat(middleware): added two middlewares for logging and security purposes

---
+++ cmd/web/middleware.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+)
+
+func SecureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Security-Policy",
+			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
+
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (app *application) LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", req.RemoteAddr, req.Proto, req.Method, req.URL.RequestURI())
+
+		next.ServeHTTP(w, req)
+	})
+}
